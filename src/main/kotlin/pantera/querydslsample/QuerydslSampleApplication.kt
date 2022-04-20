b91d1366335cec8be148298d2a1594de975b2e85
Attacktive <welcome.hell.dude@gmail.com>
2022-04-20 16:10:37 +0900

    Initial commit

---
+++ src/main/kotlin/pantera/querydslsample/QuerydslSampleApplication.kt
@@ -0,0 +1,11 @@
+package pantera.querydslsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuerydslSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuerydslSampleApplication>(*args)
+}
